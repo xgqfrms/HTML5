546d6a510a961e6d177a4ba89d56403c984b6124
xgqfrms <xgqfrms@outlook.com>
2016-06-13 12:44:30 +0800

    ok

--- PSD/js/array.js
+++ PSD/js/array.js
@@ -1,5 +1,12 @@
 /*
-Ctrl+Shift+/
+Ctrl+Shift+/  | 创建块级注释的快捷键
 */
-/*JS array tutorial*/
-var ABC = 
+
+// Ctrl+/  | 创建行级注释的快捷键
+
+
+/*JS array tutorial | JS 数组 教程*/
+
+var ABC = ['Alibaba','Baidu','Cooler'];
+// 
+var 
